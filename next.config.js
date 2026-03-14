4d9562b3fe6602b3d21f668e94cfb32f72f384a7
Cranaz <mithunjosedxb@example.com>
2026-03-14 13:00:04 +0530

    Force webpack to resolve local node_modules

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   serverExternalPackages: ['unzipper', '@babel/parser', '@babel/traverse'],
   outputFileTracingRoot: __dirname,
   typescript: {
     ignoreBuildErrors: false,
+  },
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.modules = [
+      path.resolve(__dirname, 'node_modules'),
+      ...(config.resolve.modules || []),
+    ];
+    return config;
   },
 };
 
